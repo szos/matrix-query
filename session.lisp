4c28f3826565207601a86bf08c6380b1e29735dd
szos <szos@posteo.net>
2020-01-01 23:16:40 +0100

    Added whoami and second login function - more experimentation

--- session.lisp
+++ session.lisp
@@ -2,9 +2,6 @@
 
 (in-package :matrix-query)
 
-(defparameter *homeserver* nil
-  "this is the users homeserver, where their user resides.")
-
 (defparameter *session-user-auth* nil
   "this is the users session authorization token, which is used for sending requests")
 
@@ -26,6 +23,7 @@ for example: @bob:matrix.org")
   (princ "Enter password: ")
   (read-line))
 
+(defparameter *login-info* nil)
 
 (defun login ()
   "logs in a user and stores relevant data such as access token. handles reading username
@@ -38,12 +36,11 @@ invalid-login-error and reruns. "
 				       :method :post
 				       :content-type "application/json"
 				       :content (concatenate 'string
-							     "{\"type\":"
-							     "\"m.login.password\", "
-							     "\"user\":\"" un
-							     "\", \"password\":\"" pw
-							     "\", \"device_id\":"
-							     "\"LISPclient\"}"))))
+				       		    "{\"type\":"
+				       		    "\"m.login.password\", "
+				       		    "\"user\":\"" un
+				       		    "\", \"password\":\"" pw
+				       		    "\"}"))))
 	     (setf (flexi-streams:flexi-stream-external-format stream) :utf-8)
 	     (yason:parse stream :object-as :alist))))
     (if (not *session-user-auth*)
@@ -72,3 +69,60 @@ invalid-login-error and reruns. "
   (setf *device-id* nil)
   (setf *user-address* nil)
   :logged-out)
+
+(defun login-2 ()
+  "like login, except experiments with grabbing extra info from drakma. "
+  (flet ((send-recv-login-data (un pw)
+	   (let ((stream
+		  (multiple-value-bind (response return-code info puri-uri
+						 stream something other-thing)
+		      (drakma:http-request "https://matrix.org/_matrix/client/r0/login"
+					   :want-stream t
+					   :method :post
+					   :content-type "application/json"
+					   :content (concatenate 'string
+								 "{\"type\":"
+								 "\"m.login.password\", "
+								 "\"user\":\"" un
+								 "\", \"password\":\"" pw
+								 "\"}"))
+		    (setf *login-info* return-code)
+		    response)))
+	     (setf (flexi-streams:flexi-stream-external-format stream) :utf-8)
+	     (yason:parse stream :object-as :alist))))
+    (if (not *session-user-auth*)
+	(handler-case 
+	    (let* ((username (read-username))
+		   (password (read-password))
+		   (data (send-recv-login-data username password))
+		   (token (cdr (assoc "access_token" data :test #'string=)))
+		   (device-id (cdr (assoc "device_id" data :test #'string=)))
+		   (user-id (cdr (assoc "user_id" data :test #'string=))))
+	      (when (equal (car data) '("error" . "Invalid password"))
+		;;(return-from login :invalid-credentials)
+		(error 'invalid-login-error :text "invalid password, try again"))
+	      (setf *session-user-auth* token)
+	      (setf *device-id* device-id)
+	      (setf *user-address* user-id))
+	  (invalid-login-error () (progn
+				    (format t "Incorrect username or password, try again~%")
+				    (login))))
+	(format nil "User ~a is already logged in" *user-address*))))
+
+(defun whoami ()
+  (multiple-value-bind (response status-code)
+      (drakma:http-request (concatenate 'string *homeserver* "_matrix/client/r0/account/whoami")
+			   :want-stream t
+			   :method :get
+			   :additional-headers
+			   `(("Authorization" . ,(concatenate 'string "Bearer "
+							      *session-user-auth*))))
+    (print status-code)
+    (yason:parse response :object-as :alist)))
+
+;; (defun change-password ()
+;;   "changes the password for the current user"
+;;   (let* ((new-pw ((lambda ()
+;; 		    (princ "Enter new password: ")
+;; 		    (read-line)))))
+;;     ))
