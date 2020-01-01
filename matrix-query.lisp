6e49b2c6f432203f8faa3ea9dd935b5daf0f2a53
szos <szos@posteo.net>
2020-01-01 18:38:53 +0100

    Added functions to disable echo in terminal with SBCL

--- matrix-query.lisp
+++ matrix-query.lisp
@@ -1,3 +1,21 @@
 ;;;; matrix-query.lisp
 
 (in-package #:matrix-query)
+
+(defun terminal-echo-on ()
+  (let ((tm (sb-posix:tcgetattr sb-sys:*tty*)))
+    (setf (sb-posix:termios-lflag tm)
+      (logior (sb-posix:termios-lflag tm) sb-posix:echo))
+    (sb-posix:tcsetattr sb-sys:*tty* sb-posix:tcsanow tm)))
+
+(defun terminal-echo-off ()
+  (let ((tm (sb-posix:tcgetattr sb-sys:*tty*)))
+    (setf (sb-posix:termios-lflag tm)
+      (logandc2 (sb-posix:termios-lflag tm) sb-posix:echo))
+    (sb-posix:tcsetattr sb-sys:*tty* sb-posix:tcsanow tm)))
+
+(defun read-silently-from-terminal ()
+  (prog2
+    (terminal-echo-off)
+    (read-line sb-sys:*tty*)
+    (terminal-echo-on)))
