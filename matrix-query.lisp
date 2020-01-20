74f03c9c461d5b7761e32f7c65191ff917baf13c
szos <szos@posteo.net>
2020-01-20 15:53:26 +0100

    modified string-case to return nil when otherwise matches without it

--- matrix-query.lisp
+++ matrix-query.lisp
@@ -2,6 +2,20 @@
 
 (in-package #:matrix-query)
 
+(defmacro string-case (string matches &body otherwise)
+  "this macro takes a string and a list of matches and what they will do, formed like so:
+\(\"match-string\" \(do this thing\)
+                   \(do this other thing\)\)
+and generates a conditional statement using string-equal to compare everything."
+  `(cond ,@(mapcar (lambda (match)
+		     "match is a list with car as the string to match against"
+		     `((string-equal ,string ,(car match))
+		       ,@(cdr match)))
+		   matches)
+	 (t ,@(or otherwise '(nil)))))
+
+;;; obsolete, dont document. 
+
 (defun terminal-echo-on ()
   "enables echoing user input while in a terminal. only functions with SBCL"
   (let ((tm (sb-posix:tcgetattr sb-sys:*tty*)))
